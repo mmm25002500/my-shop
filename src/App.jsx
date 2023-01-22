ce77fd2d20555c10144198e4cb46222c1701cf8a
mmm25002500 <marryhan25648@gmail.com>
2023-01-22 16:33:44 +0800

    First Upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-center">My-Shop</h1>
+      {/* <p className="bg-red-500 text-white">Hello Vite + React + Tailwind CSS!</p> */}
+    </div>
+  )
+}
+
+export default App
